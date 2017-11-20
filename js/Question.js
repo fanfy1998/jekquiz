6f679376207d36fc39cc1e792fc63147f9bafaa0
Raul Nogeuira <fanfynogueira@hotmail.com>
2017-11-20 23:08:56 +0000

    restore js folder

---
+++ js/Question.js
@@ -0,0 +1,8 @@
+class Question {
+  constructor(category, question, answers, correct) {
+    this.category = category;
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+  }
+}
