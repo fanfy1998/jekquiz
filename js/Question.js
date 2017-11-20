8736d6f3301a32afcf4d90fc488b3f3ee6e84b29
Raul Nogeuira <fanfynogueira@hotmail.com>
2017-11-20 22:41:54 +0000

    file cleanup

--- js/Question.js
+++
@@ -1,8 +0,0 @@
-class Question {
-  constructor(category, question, answers, correct) {
-    this.category = category;
-    this.question = question;
-    this.answers = answers;
-    this.correct = correct;
-  }
-}
