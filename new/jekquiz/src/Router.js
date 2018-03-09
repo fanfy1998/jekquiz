d87d1cda3f231d7ed6a6ff23b7f46023002d156e
Goncalo Amaral <goa.docs@gmail.com>
2018-03-09 01:26:47 +0000

    basic setup

---
+++ new/jekquiz/src/Router.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import './main.css'
+
+const Router = () => {
+  <BrowserRouter>
+
+  </BrowserRouter>
+}
+
+export default Router
