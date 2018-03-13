3e813f0bb8702d65e7cfd7360f693a3baa151f24
Goncalo Amaral <goa.docs@gmail.com>
2018-03-13 00:38:06 +0000

    Created home screen boilerplate

--- new/jekquiz/src/Router.js
+++ new/jekquiz/src/Router.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './main.css'
+import Screens from './screens'
 
-const Router = () => {
+const Router = () => (
   <BrowserRouter>
-
+    <Screens.Home />
   </BrowserRouter>
-}
+)
 
 export default Router
--- new/jekquiz/src/index.js
+++ new/jekquiz/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Router from './Router';
 import registerServiceWorker from './registerServiceWorker';
+
+console.log(Router);
+
 
 ReactDOM.render(<Router />, document.getElementById('root'));
 registerServiceWorker();
