a0e765cb461bd53cb97ca11c65686cafd6c947cd
Goncalo Amaral <goa.docs@gmail.com>
2018-03-18 17:32:17 +0000

    Started screen

--- new/jekquiz/src/screens/Home.js
+++ new/jekquiz/src/screens/Home.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
+const styles = {
+  section: {
+    backgroundImage: "url('images/background1.png')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    display: "flex",
+    flexDirection: "column",
+    height: "100vh",
+    width: "100%"
+  },
+  div: {
+    color: "#7CCCED",
+    display: "flex",
+    fontSize: "5.5em",
+    justifyContent: "center",
+    paddingTop: "156px",
+    fontFamily: "myFirstFont"
+  }
+}
+
 const Home = () => (
-  <div>
-    <h1>Home</h1>
-  </div>
+  <section style={styles.section}>
+    <div style={styles.div}>
+      <h1>jeKquiz</h1>
+    </div>
+  </section>
 )
 
 export default Home;
